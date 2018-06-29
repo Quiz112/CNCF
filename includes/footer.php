7ad95e0373a9ed4e2f36d88510a27d22afe3bce1
Quiz112 <savilian2000@gmail.com>
2018-06-28 21:27:22 -0400

    Final Version

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,22 @@
-	<div class="container">	
-		<hr class="featurette-divider">
-		<footer>
-			<p>This site is not official and is an assignment for a UCF Digital Media course</p>
-		</footer>
-	</div>
-  </body>
+<footer>
+
+
+	<p>Copyright© 2017 Children's Neuroblastoma Cancer Foundation. All rights reserved. </p>
+	<p>Children's Neuroblastoma Cancer Foundation is a 501(c)3 tax-exempt organization. </p>
+
+	<ul>
+		<li><a href="#"><img src="./img/socialinsta.png" class="socialIcon"></a></li>
+		<li><a href="#"><img src="./img/socialfb.png" class="socialIcon"></a></li>
+		<li><a href="#"><img src="./img/socialtwitter.png" class="socialIcon"></a></li>
+	</ul>
+	
+	<ul class="footer-links">
+		<li><a href="#">Terms & Conditions</a></li>
+		<li><a href="#">Privacy Statement</a></li>
+		<li><a href="#">Site map</a></li>
+		<li><a href="#">Feedback</a></li>
+		<li><a href="#">RSS</a></li>
+	</ul>
+	
+</footer>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <title>Children's Neuroblastoma Cancer Foundation</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <link rel="shortcut icon" href="./img/favi.png">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Reset Stylesheet -->
     <link rel="stylesheet" href="css/reset.css">
@@ -18,40 +19,67 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Main CSS -->
     <link rel="stylesheet" href="css/styles.css">
-	<!-- Main JS -->
-	<script src="js/scripts.js"></script>
+	
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/assets/owl.theme.default.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Main JS -->
+    <script src="js/scripts.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC:400,700" rel="stylesheet">
 </head>
 
 <body>
 
     <section class="logo">
-        <a class="navbar-brand" href="/home.php"><img src="http://www.cncfhope.org/site.cncfhope.org/images/logo.png" alt="Children's Neuroblastoma Cancer Foundation"></a>
+        <a class="navbar-brand" href="home.php"><img src="./img/logo_c.png" alt="Children's Neuroblastoma Cancer Foundation" style="max-width: 50vw;"></a>
     </section>
+    <p id="donate-header"><a href="donate.php" class="btn btn-warning btn-large" style="background-color: #007b9c; border: none; color: white;
+    text-shadow: 2px 2px #333; float: right; margin-top: -14%;padding: 15px;">Donate Now »</a></p>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning sticky-top">
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color: #feba03;">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav d-flex justify-content-around">
                 <li class="nav-item">
-                    <a class="nav-link" href="/home.php">Home</a>
+                    <a class="nav-link" href="home.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/about.php">About</a>
+                    <a class="nav-link" href="about.php">About</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/patients_and_families.php">Patients &amp; Families</a>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Patients &amp; Families</a>
+                    <ul class="dropdown-menu bg-warning">
+                        <li><a href="patients-and-families.php">Introduction</a></li>
+                        <li><a href="./what-is-neuroblastoma.php">What is Neuroblastoma?</a></li>
+                        <li><a href="./how-is-neuroblastoma-diagnosed.php">How is Neuroblastoma diagnosed?</a></li>
+                        <li><a href="./staging-neuroblastoma.php">Staging Neuroblastoma</a></li>
+                        <li><a href="./treatments-side-effects.php">Treatments & side effects</a></li>
+                        <li><a href="./impacts.php">Impacts</a></li>
+                    </ul>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/friends_and_communities.php">Friends &amp; Communities</a>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Friends &amp; Communities</a>
+                    <ul class="dropdown-menu bg-warning">
+                        <li><a href="friends-and-community.php">Introduction</a></li>
+                        <li><a href="community-caring.php">Community Caring</a></li>
+                        <li><a href="donate.php">Join the Cause</a></li>
+                        <li><a href="fundraising-ideas.php">Fundraising Ideas</a></li>
+                    </ul>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/doctors_and_researchers.php">Doctors &amp; Researchers</a>
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Doctors &amp; Resources</a>
+                    <ul class="dropdown-menu bg-warning">
+                        <li><a href="doctors.php">Doctors</a></li>
+                        <li><a href="resources.php">Resources</a></li>
+                    </ul>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/resources.php">Resources</a>
-                </li>
+               
             </ul>
         </div>
     </nav>
